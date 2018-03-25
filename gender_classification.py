caa4a4feb8bdcc56739d431c4129fdea8cc8355f
Muhammad Tariq Aijaz <tariq.aijaz99@gmail.com>
2018-03-25 17:53:33 +0500

    practice challenge 1 added

---
+++ gender_classification.py
@@ -0,0 +1,18 @@
+#Data Science practice challenge code
+
+from sklearn import tree
+
+classifier = tree.DecisionTreeClassifier()
+#[height, weight, shoe size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39], [177, 70, 40], [159, 55, 37], [171, 75, 42], 
+     [181, 85, 43]]
+
+Y = ['male','female','male','male','female','male','female','female','male','female','male']
+
+classifier = classifier.fit(X,Y)
+
+prediction = classifier.predict([[190,70,43]])
+
+print(prediction)
+
